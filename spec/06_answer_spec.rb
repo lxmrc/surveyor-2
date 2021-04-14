6ce5162b8fda22edb18530e2b5c5924ac54de3b8
Alex Mercea <alex@lxmrc.com>
2021-04-14 13:24:15 +1000

    Ensure answer is initialized with an instance of Question

--- spec/06_answer_spec.rb
+++ spec/06_answer_spec.rb
@@ -1,15 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe Surveyor::Answer do
-  let(:question) { double(:question, title: "Do you like your job?") }
+  let(:question) { Surveyor::Question.new(title: "Do you like your job?") }
   subject { described_class.new(question: question, value: "It's okay.") }
 
   it "has a question" do
     expect(subject.question).to eq(question)
+    expect(subject.question).to be_a(Surveyor::Question)
   end
 
   it "can't be created without a question" do
     expect { Surveyor::Answer.new }.to raise_error(ArgumentError)
+  end
+
+  it "question must be an instance of Surveyor::Question" do
+    expect { Surveyor::Answer.new(question: "Can a question just be a string?") }.to raise_error(ArgumentError)
   end
 
   it "can have an answer" do
